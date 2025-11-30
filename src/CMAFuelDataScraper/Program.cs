1ba87083ceb5f8d4756462adad028d1d354db39c
JackGilmore <46202639+JackGilmore@users.noreply.github.com>
2025-11-30 16:31:14 +0000

    chore: App skeleton

--- src/CMAFuelDataScraper/Program.cs
+++ src/CMAFuelDataScraper/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace CMAFuelDataScraper
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CMAFuelDataScraper
 {
     internal class Program
     {
+        private static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("CMA Fuel Data Scraper started.");
+
+            var cmaFuelUrl = Configuration["CMAFuelUrl"];
+
+            ArgumentException.ThrowIfNullOrEmpty(cmaFuelUrl, nameof(cmaFuelUrl));
         }
     }
 }
